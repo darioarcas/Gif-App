6db5b013c39e3f6e38ff396e5db5871084a28598
Dario Arcas <darioarcas@hotmail.com>
2024-08-07 12:25:15 -0300

    URL Modificada con HTTPS

--- src/helpers/GetGifs.js
+++ src/helpers/GetGifs.js
@@ -4,7 +4,7 @@
 
 
 const GetGifs = async(categoria)=>{
-    const url = `http://api.giphy.com/v1/gifs/search?q=${encodeURI(categoria)}&limit=10&api_key=2mDs2vqE1xoRWP52u9pKWRNtj28zd0WI`;
+    const url = `https://api.giphy.com/v1/gifs/search?q=${encodeURI(categoria)}&limit=10&api_key=2mDs2vqE1xoRWP52u9pKWRNtj28zd0WI`;
     const resp = await fetch(url);
     const {data} = await resp.json();
 
